892778452aa760e5ba7f93a386d8053506c4dca8
ousnik <42760850+ousnik@users.noreply.github.com>
2020-04-19 18:54:16 +0530

    initial commit

---
+++ web/tutorial_details.py
@@ -0,0 +1,23 @@
+list=[ "Word File using MS Word",
+       "PowerPoint Presentation using MS PowerPoint", 
+       "Poster or Invitation using Adobe Illustrator",
+       "Image Changing using Adobe Photoshop",
+       "Different layers of Images using Adobe Photoshop",
+       "Bouncing Ball animation using Adobe Flash",
+       "Moving Clouds animation using Adobe Flash",
+       "Birds Flying animation using Adobe Flash",
+       "Personal Blog using Adobe Dreamweaver",
+       "Include all the above tutorials into a web page using Adobe Dreamweaver"
+    ]
+
+path=[ r"../static/tutorials/WordDocument.docx",
+       r"../static/tutorials/PowerPointPresentation.pptx",
+       r"../static/tutorials/Poster.jpg",
+       r"../static/tutorials/ImageChanging.jpg",
+       r"../static/tutorials/DifferentLayers.jpg",
+       r"../static/tutorials/BouncingBall.gif",
+       r"../static/tutorials/MovingClouds.gif",
+       r"../static/tutorials/FlyingBird.gif",
+       r"https://my-pers-blog.herokuapp.com/",
+       r"/",
+    ]
